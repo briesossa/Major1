45c19bf250cf018faabf45daf33e907851fbebc2
Silencieux <147357836+Silencieuxx@users.noreply.github.com>
2025-06-22 22:59:03 -0500

    Added "unsigned"

--- endian.c
+++ endian.c
@@ -4,9 +4,9 @@
 //This endian swap function takes in a 32-bit integer value and finds the four bytes within the number and performs an endian swap on it.
 //Bytes 0 and 3 are swapped and then bytes 1 and 2 are swapped. The bytes are then concatenated together in a new swappedVal int and that value is returned.
 
-int endianSwap(int unswappedVal)
+unsigned int endianSwap(int unswappedVal)
 {
-    int swappedVal;
+    unsigned int swappedVal;
 
     int B0 = (unswappedVal & 0xFF000000) >> 24; //swaps B0 and B3
     int B3 = (unswappedVal & 0x000000FF) << 24;
